ed5fbd9a871f58c876358be57c6d718a867c4423
Zainav <iamzainavparveen543@gmail.com>
2025-06-25 16:52:09 +0530

    added java file

---
+++ InheritClass1.java
@@ -0,0 +1,25 @@
+class Animal {
+    void eat() {
+        System.out.println("the animal eating");
+    }
+}
+    class Dog extends Animal {
+        void bark() {
+            System.out.println("dog says bow bow");
+        }
+    }
+
+    class BabyDog extends Dog {
+        void sleep() {
+            System.out.println("baby dog weeping");
+        }
+    }
+    class Check {
+        public static void main(String[] args) {
+            BabyDog myObj = new BabyDog();
+            myObj.sleep();
+            myObj.bark();
+            myObj.eat();
+        }
+    }
+
